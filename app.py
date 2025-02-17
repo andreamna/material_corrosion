684aa6a28f2bd8d0b5fc1891dd9c271c413b782e
Takibay Sultan <takibaysultan@gmail.com>
2025-02-17 11:45:51 +0900

    Fix Gradio deployment for Railway

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import gradio as gr
+import os
 
 def greet(name):
     return "Hello " + name + "!!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
 
+# Get Railway's assigned port (default to 7860 if not set)
+port = int(os.environ.get("PORT", 7860))
 
-
+# Launch Gradio on 0.0.0.0 to allow external access
+demo.launch(server_name="0.0.0.0", server_port=port)
